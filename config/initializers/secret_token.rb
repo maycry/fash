3d4f2035a17fabf032698454f0fb757080e4d33a
maycry <byorickq@gmail.com>
2012-06-29 00:30:15 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fash::Application.config.secret_token = '6e38d578b118cd0b6a9e5a937a9c8c01202f247ecfec7972aab49759a68f198b94647780686cc750eaaa0c46e6018ab1a12acf4e7ea8363991e7e5c19cac78ea'
